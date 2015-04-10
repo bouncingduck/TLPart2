8a43d353fe475fe59ab446f5d406d14cafd577e1
Daniel Hausmann <bouncingduck@gmail.com>
2015-04-10 12:25:17 -0400

    Refactored example1.rb

--- example1.rb
+++ example1.rb
@@ -3,15 +3,15 @@
 puts "What's your age, little one?"
 age = gets.chomp.to_i
 
-case age
+answer = case age
 when 5
-  puts 'idiot young\'un'
+  'idiot young\'un'
 when 6
-  puts 'still too young'
+  'still too young'
 when 7
-  puts 'okay now you\'re getting a little older, which is better'
+  'okay now you\'re getting a little older, which is better'
 when 8
-  puts 'you may proceed.'
+  'you may proceed.'
 end
 
-
+puts answer
