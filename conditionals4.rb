728096edfdd396040a4ace112c37282104744014
Daniel Hausmann <bouncingduck@gmail.com>
2015-04-10 17:18:58 -0400

    Combined exercises 4 and 2 into conditionals4.rb

--- conditionals4.rb
+++ conditionals4.rb
@@ -1,6 +1,7 @@
 # Conditionals Exercise 3
 
-def location(number)
+# Takes a user's number input and tells him what range it's in using case conditional.
+def locationcase(number)
   answer = case 
   when number >= 0 && number <= 50
     "#{number} is between 0 and 50"
@@ -14,7 +15,24 @@ def location(number)
   puts answer
 end
 
-puts "Type a number from 0 to a kajillion"
+# Takes a user's number input and tells him what range it's in using if conditionals.
+def locationif(number)
+  if number >= 0 && number <= 50
+    puts "#{number} is between 0 and 50"
+  elsif number >50 && number <=100
+    puts "#{number} is between 50 and 100"
+  elsif number > 100
+    puts "#{number} is greater than 100"
+  else
+    puts "#{number} is not in the preferred range, doofus.  Fix it."
+  end
+end
+
+puts "Type a number from 0 to a kajillion and I'll use the locationcase method to solve it"
 number = gets.chomp.to_i
-location(number)
+locationcase(number)
+
+puts "Type a number from 0 to a kajillion and i'll use locationif method to solve it."
+number = gets.chomp.to_i
+locationif(number)
 
