e4a4b7a0aca440863760a8792eaa4cc3aba6c1b8
Daniel Hausmann <bouncingduck@gmail.com>
2015-04-10 12:18:18 -0400

    Added my first exercise which is all about using cases as conditionals

---
+++ example1.rb
@@ -0,0 +1,17 @@
+# This is to learn about cases
+
+puts "What's your age, little one?"
+age = gets.chomp.to_i
+
+case age
+when 5
+  puts 'idiot young\'un'
+when 6
+  puts 'still too young'
+when 7
+  puts 'okay now you\'re getting a little older, which is better'
+when 8
+  puts 'you may proceed.'
+end
+
+
